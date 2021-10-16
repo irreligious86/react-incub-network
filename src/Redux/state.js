aa04bf65c64a0758aeb778924cffc1346db48dc3
Alexxs <irreligious86@gmail.com>
2021-10-17 01:50:16 +0200

    change some colors in styles to grey

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,4 +1,9 @@
 
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
+
+
 let store = {
 
     _state: {
@@ -42,7 +47,7 @@ let store = {
     },
 
     dispatch (action) {
-        if ( action.type === "ADD-POST" ) {
+        if ( action.type === ADD_POST ) {
             let newPost = {
                 id: 58,
                 message: this._state.newPostText,
@@ -51,7 +56,7 @@ let store = {
             this._state.profile.posts.push(newPost);
             this._state.newPostText = '';
             this._callSubscriber(this._state);
-        } else if ( action.type === "UPDATE-NEW-POST-TEXT" ) {
+        } else if ( action.type === UPDATE_NEW_POST_TEXT ) {
             this._state.newPostText = action.newText;
             this._callSubscriber(this._state);
         }
@@ -60,18 +65,11 @@ let store = {
 
 
 
-export const addPostActionCreator = () => {
-    return {
-        type: 'ADD-POST'
-    }
-}
-
-export const updateNewPostTextActionCreator = (text) => {
-    return {
-        type: 'UPDATE-NEW-POST-TEXT',
+export const addPostActionCreator = () => ({type: ADD_POST});
+export const updateNewPostTextActionCreator = (text) => ({
+        type: UPDATE_NEW_POST_TEXT,
         newText: text
-    }
-}
+    })
 
 
 export default store;
